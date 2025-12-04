b0f8446317b0a2677e6ae0bdef661a6a64f64cc2
JavokhirKuchkanboev <bekjavokhir14@icloud.com>
2025-12-04 13:11:53 +0500

    Add files via upload

---
+++ BandNameGenerater.py
@@ -0,0 +1,8 @@
+
+## Projecet: Band Name Generator. Needs to generate random band names based on user input. Need to know Printing, Inputting, Commenting, Debugging, name errors, sytax errors,
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+band_name = city + " " + pet
+print("Your band name could be " + band_name)
+
